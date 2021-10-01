512066dc8143746e79230cbac466da64dabb69cf
Nominerdene <nmnrdn@yahoo.com>
2021-10-01 15:26:13 +0800

    First public release

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -18,26 +18,26 @@ export default class Header extends React.Component {
                 collapseOnSelect={true}>
           <div className="container">
             <a className="navbar-brand js-scroll-trigger" href="#page-top" onClick={Scroller.handleAnchorScroll}>
-              <img src={logo} alt="Logo" />
+              <img src={logo} alt="Logo" href="#page-top" />
             </a>
             
             <Navbar.Toggle aria-controls="navbarResponsive"/>
             <Navbar.Collapse id="navbarResponsive">
-              <Nav className="navbar-nav ml-auto my-2 my-lg-0">
+              <Nav className="navbar-nav my-2 my-lg-0 ms-auto ">
                 <Scrollspy className="navbar-nav"
-                           items={["about", "services", "portfolio", "contact"]}
+                           items={["products", "services", "portfolio", "contact"]}
                            currentClassName="active" rootEl={"#mainNav"} offset={-75}>
                   <li className="nav-item">
-                    <Nav.Link className={"js-scroll-trigger"} href="#about" onClick={Scroller.handleAnchorScroll}>About</Nav.Link>
+                    <Nav.Link className={"js-scroll-trigger"} href="#products" onClick={Scroller.handleAnchorScroll}>Бүтээгдэхүүн</Nav.Link>
                   </li>
                   <li className="nav-item">
-                    <Nav.Link className={"js-scroll-trigger"} href="#services" onClick={Scroller.handleAnchorScroll}>Services</Nav.Link>
+                    <Nav.Link className={"js-scroll-trigger"} href="#services" onClick={Scroller.handleAnchorScroll}>Үйлчилгээ</Nav.Link>
                   </li>
+                  {/* <li className="nav-item">
+                    <Nav.Link className={"js-scroll-trigger"} href="#portfolio" onClick={Scroller.handleAnchorScroll}>Нэвтэрүүлсэн туршлагууд</Nav.Link>
+                  </li> */}
                   <li className="nav-item">
-                    <Nav.Link className={"js-scroll-trigger"} href="#portfolio" onClick={Scroller.handleAnchorScroll}>Portfolio</Nav.Link>
-                  </li>
-                  <li className="nav-item">
-                    <Nav.Link className={"js-scroll-trigger"} href="#contact" onClick={Scroller.handleAnchorScroll}>Contact</Nav.Link>
+                    <Nav.Link className={"js-scroll-trigger"} href="#contact" onClick={Scroller.handleAnchorScroll}>Холбоо барих</Nav.Link>
                   </li>
                 </Scrollspy>
               </Nav>
@@ -48,14 +48,12 @@ export default class Header extends React.Component {
           <div className="container h-100">
             <div className="row h-100 align-items-center justify-content-center text-center">
               <div className="col-lg-10 align-self-end">
-                <h1 className="text-uppercase text-white font-weight-bold">Your Favorite Source of Free Bootstrap
-                  Themes</h1>
+                <h1 className="text-uppercase text-white font-weight-bold">Компаний үйл ажиллагааг дэмжих програмууд</h1>
                 <hr className="divider my-4"/>
               </div>
               <div className="col-lg-8 align-self-baseline">
-                <p className="text-white-75 font-weight-light mb-5">Start Bootstrap can help you build better websites
-                  using the Bootstrap framework! Just download a theme and start customizing, no strings attached!</p>
-                <a className="btn btn-primary btn-xl js-scroll-trigger" href="#about" onClick={Scroller.handleAnchorScroll}>Find Out More</a>
+                <p className="text-white-75 font-weight-light mb-5">CRM (Хэрэглэгчтэй Харилцах Удирдлага), Ecommerce (Онлайн худалдаа), HR Management (Хүний нөөцийн удирдлага), PMS (Бүтээгдэхүүн Удирдлагын Систем)</p>
+                <a className="btn btn-primary btn-xl js-scroll-trigger" href="#products" onClick={Scroller.handleAnchorScroll}>Дэлгэрэнгүй мэдээлэл</a>
               </div>
             </div>
           </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
+import { GatsbyImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -36,19 +37,46 @@ export default class IndexPage extends React.Component {
     return (
       <Layout>
         <SEO title="Home"/>
-        <section className="page-section bg-primary" id="about">
+        <section className="page-section" id="products">
           <div className="container">
             <div className="row justify-content-center">
-              <div className="col-lg-8 text-center">
-                <h2 className="text-white mt-0">We've got what you need!</h2>
-                <hr className="divider light my-4"/>
-                <p className="text-white-50 mb-4">Start Bootstrap has everything you need to get your new website up
-                  and
-                  running in no time! Choose one of our open source, free to download, and easy to use themes! No
-                  strings
-                  attached!</p>
-                <a className="btn btn-light btn-xl js-scroll-trigger" href="#services"
-                   onClick={Scroller.handleAnchorScroll}>Get Started!</a>
+              <div className="col-lg-6 d-none d-lg-block">
+                <GatsbyImage image={this.props.data.hrimage.childImageSharp.gatsbyImageData} />
+              </div>
+              <div className="col-lg-6 col-md-12">
+                <div className="mt-5">
+                  <h3 className="h4 mb-2">HRM (Хүний нөөцийн удирдлага)</h3>
+                  <p className="text-muted mb-0">Ажилтны бүх төрлийн мэдээллийн бүртгэл, 
+                    Сургалт хөгжлийн бүртгэл, Байгууллагын бүтэц, орон тооны төлөвлөгөө, 
+                    Тушаал шийдвэрийн бүртгэл, Тайлан графикууд
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="row justify-content-center">
+              <div className="col-lg-6 col-md-12">
+                <div className="mt-5">
+                  <h3 className="h4 mb-2">CRM (Хэрэглэгчтэй Харилцах Удирдлага)</h3>
+                  <p className="text-muted mb-0">Хэрэглэгчидтэй харилцсан бүх төрлийн 
+                    бүртгэлийг хөтөлсөнөөр борлуулалтын багт хэрэглэгчээ 360 бүхлээр нь 
+                    харж манлайлах боломжийг олгодог
+                  </p>
+                </div>
+              </div>
+              <div className="col-lg-6 d-none d-lg-block">
+                <GatsbyImage image={this.props.data.crmimage.childImageSharp.gatsbyImageData} />
+              </div>
+            </div>
+            <div className="row justify-content-center">
+              <div className="col-lg-6 d-none d-lg-block">
+                <GatsbyImage image={this.props.data.ecommerceimage.childImageSharp.gatsbyImageData} />
+              </div>
+              <div className="col-lg-6 col-md-12">
+                <div className="mt-5">
+                  <h3 className="h4 mb-2">Ecommerce (Онлайн худалдаа)</h3>
+                  <p className="text-muted mb-0">Онлайн худалдааны боломжыг өөрийн байгууллагад нэвтрүүлээрэй
+                  </p>
+                </div>
               </div>
             </div>
           </div>
@@ -56,130 +84,46 @@ export default class IndexPage extends React.Component {
 
         <section className="page-section" id="services">
           <div className="container">
-            <h2 className="text-center mt-0">At Your Service</h2>
+            <h2 className="text-center mt-0">Үйлчилгээ</h2>
             <hr className="divider my-4"/>
             <div className="row">
               <div className="col-lg-3 col-md-6 text-center">
                 <div className="mt-5">
                   <i className="fas fa-4x fa-gem text-primary mb-4"></i>
-                  <h3 className="h4 mb-2">Sturdy Themes</h3>
-                  <p className="text-muted mb-0">Our themes are updated regularly to keep them bug free!</p>
+                  <h3 className="h4 mb-2">Захиалгат өөрчлөлт</h3>
+                  <p className="text-muted mb-0">Танай байгууллагын онцлогт тохируулан нэмэлт модуль хөгжүүлэлт хийх</p>
                 </div>
               </div>
               <div className="col-lg-3 col-md-6 text-center">
                 <div className="mt-5">
                   <i className="fas fa-4x fa-laptop-code text-primary mb-4"></i>
-                  <h3 className="h4 mb-2">Up to Date</h3>
-                  <p className="text-muted mb-0">All dependencies are kept current to keep things fresh.</p>
+                  <h3 className="h4 mb-2">Байнгын сайжруулалт</h3>
+                  <p className="text-muted mb-0">Бид бүтээгдэхүүнүүдээ байнга сайжруулалт хийдэг бөгөөд ашиглаж байх хугацаанд автоматаар шинэчлэгдэх болно</p>
                 </div>
               </div>
               <div className="col-lg-3 col-md-6 text-center">
                 <div className="mt-5">
                   <i className="fas fa-4x fa-globe text-primary mb-4"></i>
-                  <h3 className="h4 mb-2">Ready to Publish</h3>
-                  <p className="text-muted mb-0">You can use this design as is, or you can make changes!</p>
+                  <h3 className="h4 mb-2">Олон хэлний боломж</h3>
+                  <p className="text-muted mb-0">Програм нь Монгол, Англи хэлний сонголттой. Хэрэглэгч бүр өөрийн хэлний сонголтоо тохируулж ашиглах боломжтой.</p>
                 </div>
               </div>
               <div className="col-lg-3 col-md-6 text-center">
                 <div className="mt-5">
                   <i className="fas fa-4x fa-heart text-primary mb-4"></i>
-                  <h3 className="h4 mb-2">Made with Love</h3>
-                  <p className="text-muted mb-0">Is it really open source if it's not made with love?</p>
+                  <h3 className="h4 mb-2">Холболт хийх боломж</h3>
+                  <p className="text-muted mb-0">Байгууллагад ашиглаж байгаа системүүдтэй холболт хийх бүрэн боломжтой </p>
                 </div>
               </div>
             </div>
           </div>
         </section>
 
-        <section id="portfolio">
+        <section className="page-section bg-dark text-white" id="portfolio">
           <div className="container-fluid p-0">
             <div className="row no-gutters">
-              <div className="col-lg-4 col-sm-6">
-                <a className="portfolio-box" href="img/portfolio/fullsize/1.jpg" onClick={this.handlePortfolioClick.bind(this, 0)}>
-                  <Img fluid={this.props.data.images.edges[0].node.childImageSharp.fluid}/>
-                  <div className="portfolio-box-caption">
-                    <div className="project-category text-white-50">
-                      Category
-                    </div>
-                    <div className="project-name">
-                      Project Name
-                    </div>
-                  </div>
-                </a>
-              </div>
-              <div className="col-lg-4 col-sm-6">
-                <a className="portfolio-box" href="img/portfolio/fullsize/2.jpg" onClick={this.handlePortfolioClick.bind(this, 1)}>
-                  <Img fluid={this.props.data.images.edges[1].node.childImageSharp.fluid}/>
-                  <div className="portfolio-box-caption">
-                    <div className="project-category text-white-50">
-                      Category
-                    </div>
-                    <div className="project-name">
-                      Project Name
-                    </div>
-                  </div>
-                </a>
-              </div>
-              <div className="col-lg-4 col-sm-6">
-                <a className="portfolio-box" href="img/portfolio/fullsize/3.jpg" onClick={this.handlePortfolioClick.bind(this, 2)}>
-                  <Img fluid={this.props.data.images.edges[2].node.childImageSharp.fluid}/>
-                  <div className="portfolio-box-caption">
-                    <div className="project-category text-white-50">
-                      Category
-                    </div>
-                    <div className="project-name">
-                      Project Name
-                    </div>
-                  </div>
-                </a>
-              </div>
-              <div className="col-lg-4 col-sm-6">
-                <a className="portfolio-box" href="images/portfolio/fullsize/4.jpg" onClick={this.handlePortfolioClick.bind(this, 3)}>
-                  <Img fluid={this.props.data.images.edges[3].node.childImageSharp.fluid}/>
-                  <div className="portfolio-box-caption">
-                    <div className="project-category text-white-50">
-                      Category
-                    </div>
-                    <div className="project-name">
-                      Project Name
-                    </div>
-                  </div>
-                </a>
-              </div>
-              <div className="col-lg-4 col-sm-6">
-                <a className="portfolio-box" href="img/portfolio/fullsize/5.jpg" onClick={this.handlePortfolioClick.bind(this, 4)}>
-                  <Img fluid={this.props.data.images.edges[4].node.childImageSharp.fluid}/>
-                  <div className="portfolio-box-caption">
-                    <div className="project-category text-white-50">
-                      Category
-                    </div>
-                    <div className="project-name">
-                      Project Name
-                    </div>
-                  </div>
-                </a>
-              </div>
-              <div className="col-lg-4 col-sm-6">
-                <a className="portfolio-box" href="img/portfolio/fullsize/6.jpg" onClick={this.handlePortfolioClick.bind(this, 5)}>
-                  <Img fluid={this.props.data.images.edges[5].node.childImageSharp.fluid}/>
-                  <div className="portfolio-box-caption p-3">
-                    <div className="project-category text-white-50">
-                      Category
-                    </div>
-                    <div className="project-name">
-                      Project Name
-                    </div>
-                  </div>
-                </a>
-              </div>
+              
             </div>
-          </div>
-        </section>
-
-        <section className="page-section bg-dark text-white">
-          <div className="container text-center">
-            <h2 className="mb-4">Free Download at Start Bootstrap!</h2>
-            <a className="btn btn-light btn-xl" href="https://startbootstrap.com/themes/creative/">Download Now!</a>
           </div>
         </section>
 
@@ -187,21 +131,19 @@ export default class IndexPage extends React.Component {
           <div className="container">
             <div className="row justify-content-center">
               <div className="col-lg-8 text-center">
-                <h2 className="mt-0">Let's Get In Touch!</h2>
+                <h2 className="mt-0">Бидэнтэй холбоо барих</h2>
                 <hr className="divider my-4"/>
-                <p className="text-muted mb-5">Ready to start your next project with us? Give us a call or send us an
-                  email
-                  and we will get back to you as soon as possible!</p>
+                <p className="text-muted mb-5">Өөрийн байгууллагын дотоод үйл ажиллагааг сайжруулах, мэдээллийн нэгдсэн бүртгэлтэй болохыг хүсвэл бидэнд хандаарай</p>
               </div>
             </div>
             <div className="row">
               <div className="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
                 <i className="fas fa-phone fa-3x mb-3 text-muted"></i>
-                <div>+1 (202) 555-0149</div>
+                <div>9910 8470</div>
               </div>
               <div className="col-lg-4 mr-auto text-center">
                 <i className="fas fa-envelope fa-3x mb-3 text-muted"></i>
-                <a className="d-block" href="mailto:contact@yourwebsite.com">contact@yourwebsite.com</a>
+                <a className="d-block" href="mailto:info@fineworks.mn">info@fineworks.mn</a>
               </div>
             </div>
           </div>
@@ -226,6 +168,30 @@ export const imageData = graphql`
             }
           }
         }
+      }
+    }
+    hrimage: file(relativePath: { eq: "human-resource-management-1024x575.png" }) {
+      childImageSharp {
+        gatsbyImageData(
+          width: 450
+          layout: FIXED
+        )
+      }
+    }
+    crmimage: file(relativePath: { eq: "complete-crm-history-header.jpg" }) {
+      childImageSharp {
+        gatsbyImageData(
+          width: 450
+          layout: FIXED
+        )
+      }
+    }
+    ecommerceimage: file(relativePath: { eq: "how-ecommerce-companies-can-care-for-their-customers-5eb56dfe6c64e-1520x800-1.png" }) {
+      childImageSharp {
+        gatsbyImageData(
+          width: 450
+          layout: FIXED
+        )
       }
     }
   }
